72e1d069032f916b682cae44eac840088beaa965
Stephen Kilonzo <skilonzo@bim-info.ru>
2021-05-08 13:30:30 +0300

    deploy module error

--- lib/queries/getRobotById.ts
+++ lib/queries/getRobotById.ts
@@ -1,1 +1,14 @@
-export const n = ""
+import { gql } from '@apollo/client';
+
+export const getQuery = () => {
+  const LOAD_ROBOTS= gql`
+    query GetRobots {
+      robots(limit:10) {
+        id
+        code      
+      }
+    }
+  `;
+   
+  return LOAD_ROBOTS;
+}
